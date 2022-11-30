5a3c4d3482559ec55d3cf4e061edad579421c412
Bradd Szonye <bszonye@gmail.com>
2022-11-29 20:03:48 -0800

    Add toml support.

--- bones/__main__.py
+++ bones/__main__.py
@@ -12,24 +12,36 @@ import sys
 from fractions import Fraction
 
 import lea
-import matplotlib
 from lea.leaf import dice
-from matplotlib import pyplot
 
-from bones.warhammer import Characteristic, Profile, chain_rolls
+from bones.warhammer import chain_rolls, Characteristic, Profile, Weapon
 
 
 def profile_demo() -> None:
     """Demonstrate profile classes."""
     c1 = Characteristic(name="Column 1", value=1)
     c2 = Characteristic(name="Column 2", value=2)
-    ptest = Profile("Test Profile", [c1, c2])
-    for item in ptest:
-        print(item)
+    clist = (c1, c2)
+    plist = Profile(clist, name="List Profile")
+    print(plist)
+    cdict = {c1.name: c1.value, c2.name: c2.value}
+    pdict = Profile(cdict, name="Dict Profile")
+    print(pdict)
+    ctoml = "['Column 2']\nvalue='TODO'\n['Column 3']\nvalue=3"
+    ptoml = Weapon.loads(ctoml, name="TOML Profile")
+    print(ptoml)
+    # pfile = Warscroll.load(sys.stdin.buffer, name="TOML Warscroll")
+    # print(pfile)
 
 
 def plot_demo() -> None:
     """Show a few simple placeholder demos."""
+    try:
+        import matplotlib
+        from matplotlib import pyplot
+    except ImportError:
+        return
+
     # attacks
     attacks = dice(1)
     # hit: 4+ exploding 6s
@@ -67,6 +79,7 @@ def main() -> None:
     """Script entry point. Command-line interface TBD."""
     print(__name__)
     profile_demo()
+    plot_demo()
     sys.exit(0)
 
 
--- bones/warhammer.py
+++ bones/warhammer.py
@@ -2,23 +2,36 @@
 
 __all__ = (
     "AttackCounter",
+    "Characteristic",
     "Datasheet",
+    "Profile",
+    "Unit",
     "Warscroll",
     "Weapon",
     "chain_rolls",
 )
 
 import dataclasses
+import sys
 from collections.abc import Iterable, Iterator, Mapping
-from typing import Any, NewType, SupportsFloat as Numeric, Union
+from typing import Any, IO, Optional, SupportsFloat, Type, TypeVar, Union
 
 import lea
 
+# Conditionally import tomllib (Python 3.11+) or tomli.
+# (mypy doesn't understand the try/except idiom)
+if sys.version_info[:2] < (3, 11):
+    import tomli as tomllib
+else:
+    import tomllib
+
+# Type definitions.
 # TODO: remove unused types
+Numeric = SupportsFloat  # reasonable approximation of "numeric"
 Distance = Numeric  # e.g. for move & range characteristics
-TargetNumber = NewType("TargetNumber", int)  # target for roll characteristics
-DiceModifier = NewType("DiceModifier", int)  # e.g. rend
+ProfileType = TypeVar("ProfileType", bound="Profile")
 Randomizable = Union[Numeric, lea.Lea]
+NameDict = dict[str, Any]
 
 lea.set_prob_type("r")
 
@@ -41,6 +54,10 @@ class Characteristic:
     name: str
     value: Any  # TODO: refine this? depends on the characteristic type
 
+    def __iter__(self) -> Iterator[Any]:
+        """Enable conversion to (name, value) for dict construction."""
+        return iter(dataclasses.astuple(self))
+
 
 class Profile(Mapping[str, Characteristic]):
     """Tabular data for units and weapons.
@@ -49,24 +66,66 @@ class Profile(Mapping[str, Characteristic]):
     with a named **profile** in each row and the characteristic values
     in columns.  This base class represents basic profile data with
     subscript notation for accessing the characteristics by name.
+
     """
 
-    name: str
+    name: Optional[str]
     characteristics: dict[str, Characteristic]
 
-    def __init__(self, name: str, characteristics: Iterable[Characteristic]) -> None:
+    def __init__(
+        self,
+        __c: Union[NameDict, Iterable[Any]],
+        /,
+        *,
+        name: Optional[str] = None,
+    ):
         """Initialize characteristics."""
+        # Convert input data to a characteristic dictionary.
+        # TODO: Handle specialized values (e.g. target numbers).
+        items = __c.items() if isinstance(__c, Mapping) else __c
+        characteristics = (
+            item if isinstance(item, Characteristic) else Characteristic(*item)
+            for item in items
+        )
         self.characteristics = {item.name: item for item in characteristics}
+        self.name = name
+
+    @classmethod
+    def load(
+        cls: Type[ProfileType], __fp: IO[bytes], /, *args: Any, **kwargs: Any
+    ) -> ProfileType:
+        """Construct a new Profile object from a TOML file."""
+        data = tomllib.load(__fp)
+        return cls(data, *args, **kwargs)
+
+    @classmethod
+    def loads(
+        cls: Type[ProfileType], __s: str, /, *args: Any, **kwargs: Any
+    ) -> ProfileType:
+        """Construct a new Profile object from a TOML string."""
+        data = tomllib.loads(__s)
+        return cls(data, *args, **kwargs)
 
     def __getitem__(self, key: str) -> Characteristic:
         """Subscript notation to simplify access to characteristics."""
         return self.characteristics[key]
 
     def __iter__(self) -> Iterator[str]:
+        """Iterate over the names of all characteristics."""
         return iter(self.characteristics)
 
     def __len__(self) -> int:
+        """Report the number of characteristics."""
         return len(self.characteristics)
+
+    def __repr__(self) -> str:
+        """Dump object data in repr() format."""
+        data = tuple(c for c in self.characteristics.values())
+        return "{}({}, name={})".format(
+            type(self).__name__,
+            repr(data),
+            repr(self.name),
+        )
 
 
 class Weapon(Profile):
